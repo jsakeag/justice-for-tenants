6cec8c09da7bf93419ae0457ab004daf642364a0
Keagan Aromin <kgnaromin@berkeley.edu>
2025-02-01 12:11:37 -0800

    Cleaning and restructuring

--- tenant.py
+++ tenant.py
@@ -1,51 +1,8 @@
-import os
-import json
-import re
-import requests
-from typing import Dict, Any, List, Union, Tuple
-
-import google.generativeai as genai
-from dotenv import load_dotenv
-
-from langchain import LLMChain, PromptTemplate
-from langchain.agents import (
-    Tool,
-    AgentExecutor,
-    LLMSingleActionAgent,
-    load_tools,
-    initialize_agent
-)
-from langchain.chains.summarize import load_summarize_chain
 from langchain.chains import RetrievalQA
-from langchain.document_loaders import (
-    PyPDFLoader,
-    TextLoader,
-    DirectoryLoader
-)
-from langchain.memory import ConversationBufferMemory
-from langchain.output_parsers import StructuredOutputParser, ResponseSchema
-from langchain.prompts import (
-    BaseChatPromptTemplate,
-    PromptTemplate,
-    ChatPromptTemplate,
-    HumanMessagePromptTemplate
-)
-from langchain.schema import AgentAction, AgentFinish, HumanMessage
+from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import FAISS
-
-from langchain_google_genai import (
-    ChatGoogleGenerativeAI,
-    GoogleGenerativeAIEmbeddings
-)
-from langchain_community.utilities import (
-    GoogleSearchAPIWrapper,
-    TextRequestsWrapper
-)
 from LegalDocumentProcessor import LegalDocumentProcessor
-GOOGLE_CSE_ID = os.getenv('GOOGLE_CSE_ID', '44cccd4214e594369')
-GOOGLE_SEARCH_KEY = os.getenv('GOOGLE_SEARCH_KEY', 'AIzaSyAOI--7LYnBplAtoCzYx-0lLnneHz0euso')
-GEMINI_API = "AIzaSyDYIEW4XVSeuMaVlcmXgv2rqI20jqUolwk"
 
 # First, process and load your historical hearing orders
 processor = LegalDocumentProcessor()
